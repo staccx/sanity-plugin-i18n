584616e9a9e89fd802b5c31fa63888b3efe8e872
Espen Th Sæverud <espensaeverud@gmail.com>
2019-12-18 16:13:12 +0100

    feat: add project

---
+++ babel.config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+  plugins: [
+  ],
+  ignore: [
+    "**/src/**/*.test.js",
+    "**/src/**/*.spec.js",
+    "**/src/**/*.stories.js",
+    "**/src/**/*.md",
+    "**/src/**/*.preview.js"
+  ]
+}
